b0d09962161826962d817eaef9c6bf895a68f002
RenderHaven <vikrambalai1002@gmail.com>
2026-08-24 18:11:43 +0530

    first push

--- app/config.py
+++ app/config.py
@@ -2,14 +2,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str="sqlite:///./feed_builder.db"
+    DATABASE_URL: str
     REDIS_URL: str
-    KAFKA_BOOTSTRAP_SERVERS: str = "localhost:9092"
+
+    KAFKA_BOOTSTRAP_SERVERS: str | None = None
     KAFKA_INTERACTIONS_TOPIC: str = "interactions_topic"
     KAFKA_COMMENTS_TOPIC: str = "comments_topic"
+
     REDIS_REBUILD_INTERVAL: str = "6h"
     DB_ECHO: bool = False
-    CURSOR_SECRET_KEY:str= "mDDhpSGJwzSX-YGwr7GzZMCj9icfsGxFdy_RR2esd8w="
+
+    CURSOR_SECRET_KEY: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
